cbe784b1c93a0eaef25103c162733479347f77fb
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2025-06-17 10:34:30 +0000

    change poll log levels

--- src/libbpf.c
+++ src/libbpf.c
@@ -13873,8 +13873,7 @@ int perf_buffer__poll(struct perf_buffer *pb, int timeout_ms)
 
     cnt = epoll_wait(pb->epoll_fd, pb->events, pb->cpu_cnt, timeout_ms);
     if (cnt < 0) {
-        printf("libbpf: error polling perf buffer: %d\n", errno);
-        fprintf(stderr, "libbpf: error polling perf buffer: %d\n", errno);
+        pr_info("libbpf: error polling perf buffer: %d\n", errno);
         return -errno;
     }
 
@@ -13883,9 +13882,7 @@ int perf_buffer__poll(struct perf_buffer *pb, int timeout_ms)
 
         err = perf_buffer__process_records(pb, cpu_buf);
         if (err) {
-            printf("libbpf: error while processing records: %d\n", err);
-            fprintf(stderr, "libbpf: error while processing records: %d\n", err);
-            pr_warn("error while processing records: %d\n", err);
+            pr_info("error while processing records: %d\n", err);
             return libbpf_err(err);
         }
     }
