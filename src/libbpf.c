8657071e9c1f15cb23c1913b2f9a3bb5b10ec752
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2025-06-23 11:24:16 +0000

    remove logs

--- src/libbpf.c
+++ src/libbpf.c
@@ -13873,12 +13873,10 @@ int perf_buffer__poll(struct perf_buffer *pb, int timeout_ms)
 
     cnt = epoll_wait(pb->epoll_fd, pb->events, pb->cpu_cnt, timeout_ms);
     if (cnt < 0) {
-        pr_warn("libbpf: error polling perf buffer: %d\n", errno);
         return -errno;
     }
 
     for (i = 0; i < cnt; i++) {
-        pr_warn("libbpf: processing record %d\n", i);
         struct perf_cpu_buf *cpu_buf = pb->events[i].data.ptr;
 
         err = perf_buffer__process_records(pb, cpu_buf);
